ccf996afb4da027749c587546f841c229c041a11
OneMoreTest <18390842519@163.com>
2025-05-19 10:12:17 +0800

    更改

--- src/main/java/Action/ExtractVariableAction.java
+++ src/main/java/Action/ExtractVariableAction.java
@@ -14,7 +14,6 @@ import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 
